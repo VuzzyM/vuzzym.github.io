556c8d1c119db4f2d55bd080ae1705c370a65822
Vuzzy <66425682+VuzzyM@users.noreply.github.com>
2026-04-06 14:56:50 +0300

    Update no-trailer-more.js

--- no-trailer-more.js
+++ no-trailer-more.js
@@ -8,11 +8,6 @@
             
             // Remove the "More" button
             e.object.activity.render().find('.button--options').remove();
-
-            // Hide Microphone
-            if (!$('#no-mic-style').length) {
-                $('head').append('<style id="no-mic-style">.simple-keyboard-mic,[data-skbtn="{mic}"]{display:none!important;}</style>');
-            }
         }
     });
 })();
